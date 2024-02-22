3ff4ed8a35d02f268b33269bd01d20c326073792
simran <simranag951@gmail.com>
2024-02-22 10:55:13 +0530

    changes

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -14,6 +14,7 @@ const useFetch = () => {
       try {
         const res = await axios.get(backendUrl);
         setData(res.data);
+        console.log(data, "this is data")
       } catch (err) {
         setError(err);
       }
