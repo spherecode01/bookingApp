13d50d99a9e57471db8b90e07592687678014a96
simran <simranag951@gmail.com>
2024-02-22 12:25:39 +0530

    added url

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+/*import { useEffect, useState } from "react";
 import axios from "axios";
 
 const useFetch = () => {
@@ -15,7 +15,7 @@ const useFetch = () => {
         const res = await axios.get(backendUrl);
         setData(res.data);
         console.log(data, "this is data")
-        
+
       } catch (err) {
         setError(err);
       }
@@ -28,6 +28,44 @@ const useFetch = () => {
     setLoading(true);
     try {
       const res = await axios.get(backendUrl);
+      setData(res.data);
+    } catch (err) {
+      setError(err);
+    }
+    setLoading(false);
+  };
+
+  return { data, loading, error, reFetch };
+};
+
+export default useFetch;*/
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const useFetch = (url) => { // Accept a URL parameter
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get(url); // Use the passed URL
+        setData(res.data);
+      } catch (err) {
+        setError(err);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [url]); // Depend on the URL parameter
+
+  const reFetch = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get(url); // Use the passed URL
       setData(res.data);
     } catch (err) {
       setError(err);
